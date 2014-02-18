87efe6a3f6c00a8eac6bad26e28c9a9dcacae7e5
jinwen <jinwen@factual.com>
2014-02-18 17:13:54 +0800

    fix description in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject bitcoin-trader "0.1.0-SNAPSHOT"
-  :description "A simple bitcoin trader for huobi.com"
+  :description "A simple clojure-based bitcoin trader for huobi.com"
   :url "http://github.com/jinwen/bitcoin-trader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
